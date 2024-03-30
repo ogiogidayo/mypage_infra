9fb9f30cfce6d494dcc2addbd5cf33683372bea6
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-30 12:50:40 +0900

    chore: add route table private

--- network.tf
+++ network.tf
@@ -97,3 +97,24 @@ resource "aws_route_table_association" "public-rt-1c" {
   route_table_id = aws_route_table.public-rt.id
   subnet_id = aws_subnet.public_subnet_1c.id
 }
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name"    = "${var.project}-${var.environment}-private-rt"
+    "project" = var.project
+    Env       = var.environment
+    Type      = "private"
+  }
+}
+
+resource "aws_route_table_association" "private-rt-1a" {
+  route_table_id = aws_route_table.private-rt.id
+  subnet_id = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private-rt-1c" {
+  route_table_id = aws_route_table.private-rt.id
+  subnet_id = aws_subnet.private_subnet_1c.id
+}
