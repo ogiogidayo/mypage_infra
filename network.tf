a53412aef71788142cecbc564895de4424b4e31f
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-30 12:26:23 +0900

    chore: fmt

--- network.tf
+++ network.tf
@@ -2,15 +2,15 @@
 # VPC
 # -----------------------
 resource "aws_vpc" "vpc" {
-  cidr_block = "192.168.0.0/20"
-  instance_tenancy = "default"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  cidr_block                       = "192.168.0.0/20"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
   assign_generated_ipv6_cidr_block = false
 
   tags = {
-    "Name" = "${var.project}-${var.environment}-vpc"
+    "Name"    = "${var.project}-${var.environment}-vpc"
     "project" = var.project
-    Env = var.environment
+    Env       = var.environment
   }
 }
