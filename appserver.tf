e5439dee49f723472ba759ee7a5f19afb8c2ab42
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-08 08:57:15 +0900

    feat: add ec2

--- appserver.tf
+++ appserver.tf
@@ -15,20 +15,20 @@ resource "aws_key_pair" "keypair" {
 # -----------------------
 # EC2 Instance
 # -----------------------
-#resource "aws_instance" "app_server" {
-#  ami                         = data.aws_ami.app.id
-#  instance_type               = "t2.micro"
-#  subnet_id                   = aws_subnet.public_subnet_1a.id
-#  associate_public_ip_address = true
-#  security_groups = [
-#    aws_security_group.app_sg.id,
-#    aws_security_group.opmng_sg.id,
-#  ]
-#  key_name = aws_key_pair.keypair.key_name
-#  tags = {
-#    Name    = "${var.project}-${var.environment}-app-ec2"
-#    Project = var.project
-#    Env     = var.environment
-#    Type    = "app"
-#  }
-#}
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  security_groups = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id,
+  ]
+  key_name = aws_key_pair.keypair.key_name
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-ec2"
+    Project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+}
